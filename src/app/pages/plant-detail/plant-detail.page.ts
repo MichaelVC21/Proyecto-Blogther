a955442ba82af02d65ffb522a8f2623417f74826
MaFerlujan <158771176+MaFerlujan@users.noreply.github.com>
2025-05-21 17:12:23 -0400

    hoy

--- src/app/pages/plant-detail/plant-detail.page.ts
+++ src/app/pages/plant-detail/plant-detail.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PlantData, PlantEntry, plantsData } from 'src/app/data/plantsData';
 
 
+
 @Component({
   selector: 'app-plant-detail',
   templateUrl: './plant-detail.page.html',
@@ -19,20 +20,11 @@ export class PlantDetailPage implements OnInit {
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id')!;
-    const pd = plantsData[id];
-    if (!pd) {
-      this.back();
-      return;
-    }
-    this.plant = pd;
-    this.entries = [...this.plant.entries];
-    // Selecciono la última entrada por defecto
-    this.selectedEntry = this.entries[this.entries.length - 1];
-    // Extraigo todas las ubicaciones únicas
+  ngOnInit() {
     this.uniqueLocations = Array.from(
-      new Set(this.entries.flatMap((e) => e.location))
+      new Set(
+        this.entries.flatMap((e: PlantEntry) => e.location)
+      )
     );
   }
 
